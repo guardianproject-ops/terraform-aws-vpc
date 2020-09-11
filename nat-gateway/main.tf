d7a9d9b9ec9a05c0a032a7accdcb0b9824f72a37
Abel Luck <abel@guardianproject.info>
2020-09-11 08:58:30 +0200

    fix: nat gateway eip association

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -15,7 +15,7 @@ resource "aws_eip" "nat" {
 }
 resource "aws_nat_gateway" "nat" {
   count         = var.enabled ? 1 : 0
-  allocation_id = aws_eip.nat.*.id
+  allocation_id = aws_eip.nat[0].id
   subnet_id     = var.public_subnet_id
   tags = merge(
     module.nat_label.tags,
@@ -31,7 +31,7 @@ resource "aws_nat_gateway" "nat" {
 resource "aws_route" "nat" {
   count                  = var.enabled ? 1 : 0
   route_table_id         = var.private_route_table_id
-  nat_gateway_id         = aws_nat_gateway.nat.*.id
+  nat_gateway_id         = aws_nat_gateway.nat[0].id
   destination_cidr_block = "0.0.0.0/0"
 
   timeouts {
